07e5a8b005a05405b7a530d1b27f707a7fadf266
Felipe Noronha <felipenoris@gmail.com>
2018-01-25 18:42:21 -0200

    remove keyword const inside function

--- src/Lifting.jl
+++ src/Lifting.jl
@@ -32,7 +32,7 @@ lift(::Type{NullableArray{P,Q}}) where {P,Q} = Array{P,Q}
 unlift(::Type{Array{P,Q}}) where {P,Q} = NullableArray{P,Q}
 
 function lift(x::NullableVector{T}) where {T}
-	const LEN = length(x)
+	LEN = length(x)
 	out = Vector{T}(LEN)
 	for i in 1:LEN
 		@assert !isnull(x[i]) "Cannot lift a null value."
